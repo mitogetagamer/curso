d1a12b7d58a760afc31ccc00af2a4a2d76757f58
David <david.santiagonino20@gmail.com>
2024-03-11 22:01:57 -0500

    nuevos desafios

---
+++ java/desafios.js
@@ -0,0 +1,29 @@
+//* Hacer un programa que eciba del usario un numero y mueestre si es 0 o si es positivo o si es negativo //* 
+
+//const numero1 =  prompt ("Porfavor ingrese un numero")
+
+//if (numero1 > 0) { document.write ("Su numeo es positivo");
+    
+//} if (numero1 < 0) {document.write("su numero es negativo");
+    
+//}
+//if (numero1 == 0) {document.write("su numero es cero");
+//}
+
+
+// Hacer un programa que reciba del usuario dos numeros y muestre cual es el numero mayor y cual es el menor// 
+
+const numero1 =  prompt("Ingresa un numero") 
+const numero2 = prompt ("Ingresa otro numero")
+
+
+
+ if (numero1 < numero2) {document.write(" ",numero1 , " es menor que " , numero2," por ende ") 
+
+document.write(" ",numero2  , " es mayor que " , numero1," ")
+    }
+
+if    (numero1 == numero2) 
+ {document.write("Manito son iguales nada que hacer")
+ 
+}
--- java/javascript.js
+++
@@ -1,7 +0,0 @@
-//* hacer un programa que salude por el nombre 
-
-const nombreUsuario = prompt("ingresa tu nombre")
-
-let saludo = "hola"
-
-if (true) { document.write("la sorra de ",nombreUsuario)}
